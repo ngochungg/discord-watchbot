964889858bc534351bf592b443edb9571cecb3a5
ngochungg <ngochungg16@gmail.com>
2026-03-21 00:04:09 -0700

    update code

--- cogs/alert.py
+++ cogs/alert.py
@@ -8,7 +8,7 @@ from cogs.utils.notification_msg import NotificationMsg
 ALERT_CHANNEL_ID = int(os.getenv("ALERT_CHANNEL_ID", 0))
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
-class Fail2Ban(commands.Cog):
+class Alert(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.channel_id = ALERT_CHANNEL_ID
@@ -31,7 +31,7 @@ class Fail2Ban(commands.Cog):
         site = web.TCPSite(runner, '0.0.0.0', self.port)
         await site.start()
 
-        print(f"Fail2Ban web server started on port {self.port} (Listening for Lab Node)")
+        print(f"Alert web server started on port {self.port} (Listening for Lab Node)")
 
     async def handle_alert(self, request):
         try:
@@ -77,7 +77,7 @@ class Fail2Ban(commands.Cog):
         if self.server_task:
             self.server_task.cancel()
             self.bot.loop.create_task(self.server_task.cancel())
-        print("Fail2Ban web server stopped")
+        print("Alert web server stopped")
 
 async def setup(bot):
-    await bot.add_cog(Fail2Ban(bot))
+    await bot.add_cog(Alert(bot))
